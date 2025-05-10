aed54298e23364c4ddb1871ab16f4e0b1daf8f38
Jaber <j.boudouh.21500@stu.brucity.education>
2025-05-10 02:22:52 +0200

    Ajout des vues index/show pour Review + templates + routes

--- catalogue/views/review.py
+++ catalogue/views/review.py
@@ -3,15 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from catalogue.models.review import Review
 
 def index(request):
-    reviews = Review.objects.select_related('user', 'show')
+    reviews = Review.objects.select_related('show', 'user')
+    title = "Liste des critiques"
     return render(request, 'review/index.html', {
         'reviews': reviews,
-        'title': 'Liste des critiques'
+        'title': title
     })
 
 def show(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
     return render(request, 'review/show.html', {
         'review': review,
-        'title': 'Détail de la critique'
+        'title': "Critique"
     })
